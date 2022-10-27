b4c3243292c9ebed05d470f3e6522bcb3a2e70c3
swati jain <swatij57@gmail.com>
2022-10-27 12:11:27 +0530

    Add files via upload

---
+++ src/app/Services/candidate.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Candidate } from '../Models/Candidate';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CandidateService {
+
+  CANDIDATE_DATA: Candidate[] = [
+
+  ];
+
+  constructor(private http: HttpClient) { }
+
+
+  getCandidateData() {
+    console.log("http call starting..")
+    this.http.get<Candidate[]>("https://localhost:7051/api/CandidateDeatil")
+      .subscribe((res) => { this.CANDIDATE_DATA = res })
+
+    return this.CANDIDATE_DATA;
+  }
+
+
+  removecandidate(serialno: Candidate) {
+    const index = this.CANDIDATE_DATA.indexOf(serialno);
+    if (index !== -1) {
+      this.CANDIDATE_DATA.splice(index, 1);
+    }
+    console.log(this.CANDIDATE_DATA.length);
+  }
+
+  saveCandidateDetail(cand: Candidate): Observable<Candidate> {
+    console.log(cand);
+    cand.serialnumber = this.CANDIDATE_DATA.length + 1;
+
+    console.log(JSON.stringify(cand))
+    const req = this.http.post<Candidate>("http://localhost:7051/api/CandidateDeatil/SaveCandidate", JSON.stringify(cand));
+    req.subscribe();
+    return req;
+
+
+    //this.CANDIDATE_DATA.push(cand);
+  }
+
+}
---
+++ src/app/candidate-registration/candidate-registration.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CandidateService } from '../Services/candidate.service';
+import { Candidate } from '../Models/Candidate';
+
+@Component({
+  selector: 'app-candidate-registration',
+  templateUrl: './candidate-registration.component.html',
+  styleUrls: ['./candidate-registration.component.css']
+})
+export class CandidateRegistrationComponent implements OnInit {
+
+  constructor(private candidateservice: CandidateService) { }
+  fname: string = "";
+  lname: string = "";
+  phone: number = 0;
+  address: string = "";
+  resume: string = "";
+
+
+  ngOnInit(): void {
+  }
+  setValue() {
+    console.log(this.fname);
+    var candidate = new Candidate();
+    candidate.firstName = this.fname;
+    candidate.lastName = this.lname;
+    candidate.phoneNumber = this.phone;
+    candidate.address = this.address;
+    candidate.resume = this.resume;
+    this.candidateservice.saveCandidateDetail(candidate)
+  }
+
+}
